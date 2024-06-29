3f14e1bc6cfd4399f123cbdfe24e11d43790800d
Aigars <aigarspeda@gmail.com>
2024-06-29 10:19:12 +0300

    adjusted styles for home page

--- src/app/[lang]/components/CustomerFeedback.tsx
+++ src/app/[lang]/components/CustomerFeedback.tsx
@@ -1,4 +1,9 @@
+"use client";
+import Slider, { Settings } from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import DoubleArrows from "./icons/DoubleArrows";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 interface FeaturesType {
   id: number;
@@ -13,35 +18,93 @@ interface CustomerFeedbackProps {
 }
 
 export default function CustomerFeedback({ data }: CustomerFeedbackProps) {
+  // const settings: Settings = {
+  //   dots: false,
+  //   infinite: false,
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   nextArrow: <SampleNextArrow />,
+  //   prevArrow: <SamplePrevArrow />,
+  // };
+
   return (
     <div className="container mx-auto mt-4 mb-10 md:mb-20">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-12 place-content-center">
+      <Slider
+        dots={false}
+        infinite={false}
+        slidesToShow={3}
+        slidesToScroll={1}
+        nextArrow={<SampleNextArrow />}
+        prevArrow={<SamplePrevArrow />}
+        responsive={[
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+            },
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            },
+          },
+        ]}
+      >
         {data.feature.map((item) => {
           return (
-            <div key={item.id} className="p-6 bg-gray-950 text-gray-50">
-              <DoubleArrows className="w-12 h-12 mb-4" />
-              <div className="w-full">
-                <div className="mb-8 overflow-hidden md:h-60 md:custom-clamp-10">
-                  <p title={item.feedback} className="text-base">
-                    {item.feedback}
-                  </p>
-                </div>
-                <div className="text-right">
-                  <p title={item.name} className="text-base font-bold">
-                    {item.name}
-                  </p>
-                  <p title={item.position} className="text-sm">
-                    {item.position}
-                  </p>
-                  <p title={item.company} className="text-sm">
-                    {item.company}
-                  </p>
+            <div key={item.id} className="pl-2 pr-2">
+              <div className="p-6 bg-gray-950 text-gray-50">
+                <DoubleArrows className="w-12 h-12 mb-4" />
+                <div className="w-full">
+                  <div className="mb-8 overflow-hidden md:h-60 md:custom-clamp-10">
+                    <p title={item.feedback} className="text-base">
+                      {item.feedback}
+                    </p>
+                  </div>
+                  <div className="text-right">
+                    <p title={item.name} className="text-base font-bold">
+                      {item.name}
+                    </p>
+                    <p title={item.position} className="text-sm">
+                      {item.position}
+                    </p>
+                    <p title={item.company} className="text-sm">
+                      {item.company}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           );
         })}
-      </div>
+      </Slider>
+    </div>
+  );
+}
+
+function SampleNextArrow(props: any) {
+  const { className, onClick } = props as {
+    className: string;
+    onClick: () => void;
+  };
+  return (
+    <div className={className} style={{ display: "block" }} onClick={onClick}>
+      <IoIosArrowForward className="w-8 h-8 text-gray-950" />
+    </div>
+  );
+}
+
+function SamplePrevArrow(props: unknown) {
+  const { className, onClick } = props as {
+    className: string;
+    onClick: () => void;
+  };
+  return (
+    <div className={className} style={{ display: "block" }} onClick={onClick}>
+      <IoIosArrowBack className="w-8 h-8 text-gray-950" />
     </div>
   );
 }
