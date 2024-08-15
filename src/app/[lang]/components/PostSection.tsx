f04262b48520ba11ba574f165c5f6ab3a2ada0dc
Aigars <aigarspeda@gmail.com>
2024-08-15 12:57:47 +0300

    refactor: Optimize PostSection component for better performance and readability

--- src/app/[lang]/components/PostSection.tsx
+++ src/app/[lang]/components/PostSection.tsx
@@ -55,24 +55,27 @@ export default function PostSection({ data }: PostSectionProps) {
         {filteredData.map((item) => {
           const imgUrl = getStrapiMedia(item.media.data[0].attributes.url);
           return (
-            <li key={item.id} className="group">
+            <li
+              key={item.id}
+              className="bg-black group hover:bg-dentsu-hover transition-colors h-[30rem]"
+            >
               <a
                 href={`/${urlLocale}/${item.postUrl}`}
                 target={item.isNewTab ? "_blank" : "_self"}
               >
-                {imgUrl && (
+                <div>
                   <img
-                    src={imgUrl}
+                    src={imgUrl || ""}
                     alt="our client logo"
-                    className="object-cover w-full lg:h-[400px] lg:max-h-[500px] max-h-[250px] text-white h-full"
+                    className="object-cover w-full h-[15rem] text-white"
                   />
-                )}
-                <div className="flex items-end p-6 transition-colors bg-black text-gray-50 group-hover:bg-dentsu-hover">
+                </div>
+                <div className="flex items-end p-6 transition-colors text-gray-50 ">
                   <div className="w-full">
                     <div className="mb-3 overflow-hidden h-14 custom-clamp-2">
                       <h6 title={item.title}>{item.title}</h6>
                     </div>
-                    <div className="h-24 overflow-hidden custom-clamp-4">
+                    <div className="h-24 overflow-hidden custom-clamp-5">
                       <p
                         title={item.description}
                         className="text-sm font-normal text-white"
@@ -89,6 +92,40 @@ export default function PostSection({ data }: PostSectionProps) {
                 </div>
               </a>
             </li>
+            // <li key={item.id} className="group">
+            //   <a
+            //     href={`/${urlLocale}/${item.postUrl}`}
+            //     target={item.isNewTab ? "_blank" : "_self"}
+            //   >
+            //     {imgUrl && (
+            //       <img
+            //         src={imgUrl}
+            //         alt="our client logo"
+            //         className="object-cover w-full lg:h-[400px] lg:max-h-[500px] max-h-[250px] text-white h-full"
+            //       />
+            //     )}
+            //     <div className="flex items-end p-6 transition-colors bg-black text-gray-50 group-hover:bg-dentsu-hover">
+            //       <div className="w-full">
+            //         <div className="mb-3 overflow-hidden h-14 custom-clamp-2">
+            //           <h6 title={item.title}>{item.title}</h6>
+            //         </div>
+            //         <div className="h-24 overflow-hidden custom-clamp-4">
+            //           <p
+            //             title={item.description}
+            //             className="text-sm font-normal text-white"
+            //           >
+            //             {item.description}
+            //           </p>
+            //         </div>
+            //       </div>
+            //       <div className="p-0.5 md:ml-9 ml-4">
+            //         <div className="flex items-center justify-center text-center text-white transform -rotate-90 rounded-md">
+            //           <ArrowIcon className="fill-white w-9 h-9" />
+            //         </div>
+            //       </div>
+            //     </div>
+            //   </a>
+            // </li>
           );
         })}
       </ul>
