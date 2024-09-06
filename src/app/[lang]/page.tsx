80299f7052e0ca24d6b6356b82f209d55b6f4466
Aigars <aigarspeda@gmail.com>
2024-09-06 09:28:18 +0300

    Refactor page.tsx to handle missing or invalid credentials and improve metadata generation

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -14,9 +14,9 @@ export default async function RootRoute({
         "Missing or invalid credentials. Have you created an access token using the Strapi admin panel? http://localhost:1337/admin/"
       );
 
-    if (page.data.length == 0 && params.lang !== "en") return <LangRedirect />;
+    if (page.data?.length == 0 && params.lang !== "en") return <LangRedirect />;
 
-    if (page.data.length === 0) return null;
+    if (page.data?.length === 0) return null;
     const contentSections = page.data[0].attributes.contentSections;
 
     return contentSections.map((section: any, index: number) =>
