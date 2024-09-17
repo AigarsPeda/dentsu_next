ffed3d40b1c9bbcc56d68a75c900401570421ed9
Aigars <aigarspeda@gmail.com>
2024-09-17 19:29:37 +0300

    Refactor Navbar component: Update links to use Next.js Link component for responsive design

--- src/app/[lang]/components/Navbar.tsx
+++ src/app/[lang]/components/Navbar.tsx
@@ -87,19 +87,11 @@ export default function Navbar({
             </ul>
             <div className="flex items-center justify-center ml-6 space-x-3">
               {availableLocales.map((locale) => (
-                // <Link
-                //   scroll={false}
-                //   key={locale.id}
-                //   locale={locale.code}
-                //   href={getCurrentLocaleAndReplace(path, locale.code)}
-                // >
-                <button
+                <Link
+                  scroll={false}
                   key={locale.id}
-                  onClick={() => {
-                    // setMobileMenuOpen(false);
-                    router.push(getCurrentLocaleAndReplace(locale.code));
-                  }}
-                  className=""
+                  locale={locale.code}
+                  href={getCurrentLocaleAndReplace(locale.code)}
                 >
                   <div
                     style={{
@@ -118,7 +110,7 @@ export default function Navbar({
                       locale.name
                     )}
                   </div>
-                </button>
+                </Link>
               ))}
             </div>
           </div>
@@ -168,13 +160,11 @@ export default function Navbar({
               </div>
               <div className="flex flex-col items-center justify-center gap-6 mt-6">
                 {availableLocales.map((locale) => (
-                  <button
+                  <Link
+                    scroll={false}
                     key={locale.id}
-                    onClick={() => {
-                      // setMobileMenuOpen(false);
-                      router.push(getCurrentLocaleAndReplace(locale.code));
-                    }}
-                    className=""
+                    locale={locale.code}
+                    href={getCurrentLocaleAndReplace(locale.code)}
                   >
                     <div
                       style={{
@@ -193,7 +183,7 @@ export default function Navbar({
                         locale.name
                       )}
                     </div>
-                  </button>
+                  </Link>
                 ))}
               </div>
             </DialogPanel>
