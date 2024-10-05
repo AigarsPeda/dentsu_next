2db61e7b76329b0dbc8c431abadca1191c6d5a71
Aigars <aigarspeda@gmail.com>
2024-10-05 08:03:12 +0300

    Refactor CookieBanner component: Update dialog text truncation styling

--- src/app/[lang]/components/CookieBanner.tsx
+++ src/app/[lang]/components/CookieBanner.tsx
@@ -115,7 +115,9 @@ const CookieBanner: FC<CookieBannerProps> = ({ cookieBannerData }) => {
               <DialogTitle className="pb-4 text-black">
                 {cookieBannerData?.dialogTitle}
               </DialogTitle>
-              <DialogDescription>{dialogText}</DialogDescription>
+              <DialogDescription className="overflow-y-auto text-left max-h-44">
+                {dialogText}
+              </DialogDescription>
             </DialogHeader>
             {isMoreWords && (
               <div className="flex items-center justify-start">
