065192158547a5ca8e395048857e3b72da569148
A.Peda <aigarspeda@gmail.com>
2024-10-17 10:23:22 +0300

    Refactor NewsPostSection component: Update thumbnail src handling
    - In the NewsPostSection component, the handling of the thumbnail source (src) has been updated.
    - Previously, the src was obtained from the thumbnail URL using the getStrapiMedia function.
    - Now, the src is obtained using optional chaining (?.) to handle cases where the thumbnail or its URL is null or undefined.
    - This change ensures that the src is only set if a valid thumbnail URL is available.

--- src/app/[lang]/components/NewsPostSection.tsx
+++ src/app/[lang]/components/NewsPostSection.tsx
@@ -67,7 +67,7 @@ export default function NewsPostSection({ data }: NewsPostSectionProps) {
       <section className="container grid grid-cols-1 gap-8 pb-10 mx-auto md:grid-cols-3 md:px-14 mt-14">
         {currentData.map((newsPost) => {
           const thumbnail = newsPost.thumbnail.data?.[0]?.attributes;
-          const src = thumbnail ? getStrapiMedia(thumbnail.url) : null;
+          const src = thumbnail ? getStrapiMedia(thumbnail?.url) : null;
 
           if (!src) return null;
 
