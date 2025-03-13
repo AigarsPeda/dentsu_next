554c781d8ec075f77deb42a9bcae175c934402b2
Aigars <aigarspeda@gmail.com>
2025-03-13 06:29:43 +0200

    Update page route to handle missing language parameter and remove unnecessary LangRedirect condition

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -8,15 +8,17 @@ export default async function RootRoute({
   params: { lang: string };
 }) {
   try {
-    const page = await getPageBySlug("home", params.lang);
+    const page = await getPageBySlug("home", params.lang ?? "lv");
+
     if (page.error && page.error.status == 401)
       throw new Error(
         "Missing or invalid credentials. Have you created an access token using the Strapi admin panel? http://localhost:1337/admin/"
       );
 
-    if (page.data?.length == 0 && params.lang !== "en") return <LangRedirect />;
+    // if (page.data?.length == 0 && params.lang !== "en") return <LangRedirect />;
 
     if (page.data?.length === 0) return null;
+
     const contentSections = page.data[0]?.attributes?.contentSections;
 
     return contentSections.map((section: any, index: number) =>
